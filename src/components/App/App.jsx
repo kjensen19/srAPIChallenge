1ba8cfa84d611cad5b1c5058c514072d9a5809f8
Kyle Jensen <kjensen19@gmail.com>
2022-10-22 18:15:42 -0500

    React App running, GET player is returning info

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
-
+import axios from 'axios'
 
 
 function App(){
 
+    const handleClick = () => {  axios({
+        method: 'GET',
+        url: '/api/player'
+      }).then((response) => {
+        console.log('get response: ', response.data)
+      }).catch((error) => {
+        console.log('client side get error', error)
+      })
+    }
+
     return(
-        ''
+        <button onClick={handleClick}>Test</button>
     )
 }
 
