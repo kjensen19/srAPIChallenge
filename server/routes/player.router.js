3af61a464ffb482b6ad2ec36212c0c7d9903ca18
Kyle Jensen <kjensen19@gmail.com>
2022-10-23 02:15:06 -0500

    async get route is working, building object in Node and passing it to the client side

--- server/routes/player.router.js
+++ server/routes/player.router.js
@@ -2,25 +2,146 @@ const express = require('express');
 const router = express.Router()
 const axios = require('axios');
 
-
+const playerSeason = 20182019
+// const playerObject = {
+//     playerId: 8476792,
+//     playerName:'',
+//     currentTeam: '',
+//     playerAge: '',
+//     playerNumber: '',
+//     playerPosition: '',
+//     isRookie: '',
+//     assists: '',
+//     goals: '',
+//     games: '',
+//     hits: '',
+//     points: ''
+// }
+const playerId = 8476792
 
 //GET Player Route
 
-router.get('/', (req, res) =>{
-    const playerId = 8476792
-    axios({
-        method: 'GET',
-        url: `https://statsapi.web.nhl.com/api/v1/people/${playerId}`
-    })
-    .then(result => {
-        console.log(result.data.people)
-        res.send(result.data.people)
-    })
-    .catch(err => {
-        console.log('GET failed', err)
-        res.sendStatus(500)
-    })
-})
+
+// https://statsapi.web.nhl.com/api/v1/people/8476792/stats?stats=statsSingleSeason&season=20182019
+
+
+// async function getPlayerData(){
+    
+//     console.log('player in Promise:', player)
+//     console.log('season in Promise: ', season)
+
+//      await({const playerObj = {
+//         playerId: playerId,
+//         playerName: player.fullName,
+//         currentTeam: player.currentTeam.name,
+//         playerAge: player.currentAge,
+//         playerNumber: player.primaryNumber,
+//         playerPosition: player.primaryPosition.name,
+//         isRookie: player.rookie,
+//         assists: season.assists,
+//         goals: season.goals,
+//         games: season.games,
+//         hits: season.hits,
+//         points: season.points
+//     }})
+
+//     return playerObj
+// }
+
+
+// async function getPlayerData() {
+//     const [ playerData, seasonData ] = await Promise.all([ getPlayerGeneral(), getPlayerSeason ]);
+    
+//   }
+
+// function getPlayerGeneral(){  
+//     axios({
+//         method: 'GET',
+//         url: `https://statsapi.web.nhl.com/api/v1/people/${playerId}`
+//     })
+//     .then(result => {
+//         console.log('player data: ', result.)
+//         return result.data.people[0]
+//     })
+// }
+
+// function getPlayerSeason(){
+//     axios({
+//         method: 'GET',
+//         url: `https://statsapi.web.nhl.com/api/v1/people/${playerId}/stats?stats=statsSingleSeason&season=${playerSeason}`
+//     })
+//     .then(result => {
+//         console.log('Season Data: ', result.data.stats[0].splits[0].stat)
+//         return result.data.stats[0].splits[0].stat
+//     })
+// }
+
+// async function makePlayerObject(player, season) {
+//     console.log('Player in makeObj', player)
+//     console.log('Season in makeObj', season)
+//     await player;
+//     await season;
+
+//     return playerObject}
+    const dataStore = {}
+    const playerAPI = `https://statsapi.web.nhl.com/api/v1/people/${playerId}`
+    const seasonAPI = `https://statsapi.web.nhl.com/api/v1/people/${playerId}/stats?stats=statsSingleSeason&season=${playerSeason}`
+
+    const apiResources = [playerAPI, seasonAPI]
+
+    async function getResource(resource) {
+        const { data } = await axios({
+            method: 'GET',
+            url: resource})
+        dataStore[resource] = data
+    }
+
+    async function getAllResources() {
+        const apiPromises = apiResources.map(getResource)
+        await Promise.all(apiPromises)
+    }
+
+    router.get('/', (req, res) =>{
+
+        getAllResources().then(promiseRes =>{
+            const compositeData = dataStore
+            console.log('composite data = ', compositeData)
+            const player = compositeData[playerAPI].people[0]
+            const season = compositeData[seasonAPI].stats[0].splits[0].stat
+            console.log('player', player)
+            console.log('season', season)
+            
+            const playerObject = {
+                playerId: playerId,
+                playerName: player.fullName,
+                currentTeam: player.currentTeam.name,
+                playerAge: player.currentAge,
+                playerNumber: player.primaryNumber,
+                playerPosition: player.primaryPosition.name,
+                isRookie: player.rookie,
+                assists: season.assists,
+                goals: season.goals,
+                games: season.games,
+                hits: season.hits,
+                points: season.points
+            }
+
+
+            res.send(playerObject)
+
+        })
+        
+      
+
+        
+        })
+
+
+
+
+        
+
+
 
 // app.get('/gifs', (req, res) => {
 //     axios({
--- server/routes/team.router.js
+++ server/routes/team.router.js
@@ -4,5 +4,18 @@ const axios = require('axios');
 
 
 
+// Team Pipeline - Provide a team id and season year which outputs a CSV file. The CSV should include the following:
+// Team ID
+// Team Name
+// Team Venue Name
+// Games Played
+// Wins
+// Losses
+// Points
+// Goals Per Game
+// Game Date of First Game of Season
+// Opponent Name in First Game of Season
+
+
 
 module.exports = router
